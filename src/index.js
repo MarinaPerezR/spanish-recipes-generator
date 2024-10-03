821b13b01c13f66dafd64f6bf987e1cd19c32ab3
MarinaPerezR <marina.perezrobles@gmail.com>
2024-10-03 19:01:03 +0100

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  let recipeElement = document.querySelector("#recipe");
+
+  new Typewriter("#recipe", {
+    strings: "Recipe here",
+    autoStart: true,
+    delay: 3,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("form");
+formElement.addEventListener("submit", generateRecipe);
