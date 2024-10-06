88dc638f906aaa981aeb736acdb3ac1cf7b911ea
MarinaPerezR <marina.perezrobles@gmail.com>
2024-10-06 13:42:57 +0100

    Updated the prompt and context content

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ function generateRecipe(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "40b5c70t36fead3dof40b74ca69cca04";
-  let prompt = `User instructions: Generate the name of a Spanish dish with: ${instructionsInput.value}. Also add the link after a <br> element. Sign with '-SheCodes AI-' inside a <strong> element at the end of your answer using <br><br>.`;
+  let prompt = `User instructions: Generate the name with a short explanation of a Spanish dish with: ${instructionsInput.value}. After the dish name add <br> 'Look for the whole recipe in Google.' Sign with <br> <strong> -SheCodes AI- at the end of your answer.`;
   let context =
-    "You are an expert in Spanish cuisine so with the ingredients given to you you are going to give the name of a Spanish dish you can do using those ingredients. Make sure to use the user instructions";
+    "You are an expert in Spanish cuisine so with the ingredients given to you you are going to give the name of a Spanish dish you can do using those ingredients with a short explanation. Make sure to use the user instructions";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
